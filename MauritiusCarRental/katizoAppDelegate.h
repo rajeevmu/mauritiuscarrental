7c8cd48d388e43f69f3acf7d8e82d0d77682b92e
rajeevmu <rajkhoo77@yahoo.com>
2013-02-26 22:14:37 +0400

    Initial Commit

---
+++ MauritiusCarRental/katizoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  katizoAppDelegate.h
+//  MauritiusCarRental
+//
+//  Created by admin on 2/26/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface katizoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MauritiusCarRental/katizoViewController.h
@@ -0,0 +1,13 @@
+//
+//  katizoViewController.h
+//  MauritiusCarRental
+//
+//  Created by admin on 2/26/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface katizoViewController : UIViewController
+
+@end
